9753c72e218d2f7b5bb70ee2fbfc062137aa5d16
nazo <webncyber@yahoo.com>
2023-07-16 20:05:04 -0400

    update key url names

--- src/app/api/getcontent/route.tsx
+++ src/app/api/getcontent/route.tsx
@@ -27,11 +27,11 @@ export async function GET(request: Request)
     const { searchParams } = new URL(request.url)
     let requestedPage = searchParams.get("url") != null ? searchParams.get("url") : "home";
 
-    const response = await fetch(`${process.env.API_ReadOnly_URL}`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_ReadOnly_URL}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${process.env.API_KEY}`
+          "Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
         },
         body: JSON.stringify({
           query: gql(requestedPage),
