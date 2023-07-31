886f86e592bd683a0e8b64390e43b0cb8fd2eb78
nazo <webncyber@yahoo.com>
2023-07-30 22:08:30 -0400

    revert test

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async headers() {
-        return [
-          {
-            source: '/',
-            headers: [
-              {
-                key: 'x-url',
-                value: '/'
-              }
-            ],
-          },
-        ]
-      }
-}
+const nextConfig = {}
 
 module.exports = nextConfig
