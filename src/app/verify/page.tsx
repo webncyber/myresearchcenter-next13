140bc0cb55bbaa9049a8268c73a61e6d85bf9dfc
nazo <webncyber@yahoo.com>
2023-08-08 17:41:18 -0400

    check in verify

---
+++ src/app/verify/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+
+export default async function verify() {
+ 
+
+  return (
+   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt USD192478989 ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+  )
+}
+
+
