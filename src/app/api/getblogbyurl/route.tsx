35536a0fcd12a7a615947b798f2c991d2915e136
nazo <webncyber@yahoo.com>
2024-04-27 13:57:44 -0400

    revert

--- src/app/api/getblogbyurl/route.tsx
+++ src/app/api/getblogbyurl/route.tsx
@@ -1,26 +1,26 @@
 import { NextResponse } from "next/server";
-import { gqlGetBlogByURL } from "../gql/blogQueries";
+import {gqlGetBlogByURL} from '../gql/blogQueries'
 import { revalidateAPITag } from "../../../../lib/constants";
 
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
+export async function GET(request: Request) 
+{
+  
+    const { searchParams } = new URL(request.url)
+    
+    let requestedPage = searchParams.get("url") != null ? searchParams.get("url") : "";
 
-  let requestedPage =
-    searchParams.get("url") != null ? searchParams.get("url") : "";
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_ReadOnly_URL}`, {
+      next: { tags: [revalidateAPITag] },
+      method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`
+        },
+        body: JSON.stringify({
+          query: gqlGetBlogByURL(requestedPage),
+        })
+    })
 
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_ReadOnly_URL}`, {
-    //next: { tags: [revalidateAPITag] },
-    cache: "no-store",
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY}`,
-    },
-    body: JSON.stringify({
-      query: gqlGetBlogByURL(requestedPage),
-    }),
-  });
-
-  const data = await response.json();
-  return NextResponse.json({ data });
+    const data = await response.json();
+    return NextResponse.json({data});
 }
