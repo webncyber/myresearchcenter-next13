da0b233f901ef26fe9af399dcf7663eac133ba5d
nazo <webncyber@yahoo.com>
2024-06-14 09:51:30 -0400

    add x tenant and 301 redirect

--- next.config.js
+++ next.config.js
@@ -1,5 +1,79 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/flyers/dr-strings-nickel-lo-rider',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/baby-must-haves/BabyBjorn-small-baby-bib',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/back-to-school/large-college-backpack',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/baby-must-haves/retractable-baby-gate',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/back-to-school/shower-caddy-tote-bag',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/home-decor/facts-on-having-a-cozy-home',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/healthy-recipes/top-5-brunch-ideas',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/baby-must-haves/baby-electric-nail-trimmer',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/gardening/leaf-sweepers',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/back-to-school',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/healthy-recipes',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/home-decor',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/baby-must-haves',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/blogs/gardening',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
     compiler: {
         styledComponents: true,
       }
